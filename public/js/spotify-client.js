19ae65d33da1887fe59fd31669ea532fe5f4238d
Aaron Bieber <aaron@bolddaemon.com>
2016-03-09 17:18:21 -0700

    mostly functional again

--- public/js/spotify-client.js
+++ public/js/spotify-client.js
@@ -12,323 +12,346 @@ var getPlayLists, //function
     currentTrack = 0;
 
 $(document).ready(function(){
-  if(typeof($.cookie('username')) != 'undefined')
-    $('#username').val($.cookie('username'))
+    if(typeof($.cookie('username')) != 'undefined')
+	$('#username').val($.cookie('username'))
 
-  if(typeof($.cookie('password')) != 'undefined')
-    $('#password').val($.cookie('password'));
+    if(typeof($.cookie('password')) != 'undefined')
+	$('#password').val($.cookie('password'));
 
-  $.ajax({
-    cache: false,
-    dataType: "json",
-    url: '/spotify-server/login/check:check',
-    beforeSend: function() {
-      $('.modal').modal('show');
-    }            
-  }).done(function(data){  
-    if(data.success){
-      $('#nav').show();
-      $('.login-wrapper').hide();
-      getPlayLists();        
-    }
-    else{
-      $('.login-wrapper').show();
-      $('.modal').modal('hide');
-    }       
-  }); 
+    $('.login-wrapper').show();
 
-  //Event handlers for play/pause/nextTrack/track clicks
-  $(document).click(function(e){
-    //Handler for each Playlist link
-    if(e.target.name==='playListLink'){
-      e.preventDefault();  
-      getPlayListTracks(e.target.id);
-    }
-    //Handler for playAudio button
-    else if(e.target.id==='playAudio'){
-      e.preventDefault();
-      $('#audioPlayer').get(0).play();
-    }
-    //Handler for pauseAudio button
-    else if(e.target.id==='pauseAudio'){
-      e.preventDefault();
-      $('#audioPlayer').get(0).pause();
-    }
-    //Handler for nextTrack button
-    else if(e.target.id==='nextTrack'){
-      e.preventDefault();
-      currentTrack++;
-      loadTrack();
-    }
-    else if(e.target.id==='previousTrack'){
-      e.preventDefault();
-      currentTrack--;
-      loadTrack();
-    }
-    //Handler for enqueueing a track from search
-    else if(e.target.className==='enqueueTrack'){
-      var id = e.target.id;
-      e.preventDefault();      
-      $.ajax({
-        cache: false,
-        dataType: "json",
-        url: '/spotify-server/track/'+id,
-        beforeSend: function() {
-          $('.modal').modal('show');
-        }            
-      }).done(function(data){  
-        if(data.error){
-          displayError(data.error);
-          return;
-        }
-
-        //Build custom queue
-        var firstSong = false;
-
-        if(customTrackQueue.length==0){
-          firstSong = true;
-        }
-
-        //If customTrackQueue is empty, we need to make one
-        if(typeof(tracks)=='undefined'){
-          tracks = new Object();
-        }
-
-        if(typeof(tracks.tracks)=='undefined'){
-          tracks.tracks = new Array();
-        }
-
-        customTrackQueue.push(data);        
-        tracks.tracks = customTrackQueue;
-        tracks.tracks[customTrackQueue.length-1].trackURI = id;
-
-        if(firstSong == true){
-          currentTrack = 0;
-          loadTrack();
-        }
-
-        $('.modal').modal('hide'); 
-      });        
-    }      
-  });  
-
-  getPlayLists = function() {
-    //Get Playlists
     $.ajax({
-      cache: false,
-      dataType: "json",
-      url: '/spotify-server/playlists',
-      beforeSend: function() {
-        $('.modal').modal('show');
-      }
+	cache: false,
+	dataType: "json",
+	type: 'POST',
+	data: $('#login-form').serialize(),
+	url: '/spotify-server/login',
+	beforeSend: function() {
+	    $('.modal').modal('show');
+	}
     }).done(function(data){
-      if(data.error){
-        displayError(data.error);
-        return;
-      }      
+	if(data.success){
+	    $('#nav').show();
+	    $('.login-wrapper').hide();
+	    getPlayLists();
+	}
+	else{
+	    $('.login-wrapper').show();
+	    $('.modal').modal('hide');
+	}
+    });
 
-      data.playlists.sort(sortByAttributeNameComparitor)
-      renderHandlebarsTemplate('playlists', data);  
-      playlists = data;        
-      $('.modal').modal('hide');
-    });      
-  }           
+    //Event handlers for play/pause/nextTrack/track clicks
+    $(document).click(function(e){
+	//Handler for each Playlist link
+	if(e.target.name==='playListLink'){
+	    e.preventDefault();
+	    getPlayListTracks(e.target.id);
+	}
+	//Handler for playAudio button
+	else if(e.target.id==='playAudio'){
+	    e.preventDefault();
+	    $('#audioPlayer').get(0).play();
+	}
+	//Handler for pauseAudio button
+	else if(e.target.id==='pauseAudio'){
+	    e.preventDefault();
+	    $('#audioPlayer').get(0).pause();
+	}
+	//Handler for nextTrack button
+	else if(e.target.id==='nextTrack'){
+	    e.preventDefault();
+	    currentTrack++;
+	    loadTrack();
+	}
+	else if(e.target.id==='previousTrack'){
+	    e.preventDefault();
+	    currentTrack--;
+	    loadTrack();
+	}
+	//Handler for enqueueing a track from search
+	else if(e.target.className==='enqueueTrack'){
+	    var id = e.target.id;
+	    e.preventDefault();
+	    $.ajax({
+		cache: false,
+		dataType: "json",
+		url: '/spotify-server/track/'+id,
+		beforeSend: function() {
+		    $('.modal').modal('show');
+		}
+	    }).done(function(data){
+		if(data.error){
+		    displayError(data.error);
+		    return;
+		}
 
-  //Get Plastlist tracks        
-  getPlayListTracks = function(uri){
-    $.ajax({
-      cache: false,
-      dataType: "json",
-      url: '/spotify-server/playlist/'+uri.split(':')[4],
-      beforeSend: function() {
-        $('.modal').modal('show');
-      }            
-    }).done(function(data){    
-      if(data.error){
-        displayError(data.error);
-        return;
-      }  
-              
-      renderHandlebarsTemplate('tracks', data);
+		//Build custom queue
+		var firstSong = false;
 
-      //Load first track
-      tracks = data;
-      currentTrack = 0;
-      loadTrack();
-    });          
-  }
+		if(customTrackQueue.length==0){
+		    firstSong = true;
+		}
 
-  renderAlbumArt = function(trackURI){
-    $.ajax({
-      cache: false,
-      dataType: "json",
-      url: '/spotify-server/album-art/'+trackURI        
-    }).done(function(data){
-      renderHandlebarsTemplate('currentAlbumArt', data.oembed, '#albumArt');
-    }); 
-  }
+		//If customTrackQueue is empty, we need to make one
+		if(typeof(tracks)=='undefined'){
+		    tracks = new Object();
+		}
 
-  //Play next track
-  loadTrack = function() {
-    $('.modal').modal('show');
+		if(typeof(tracks.tracks)=='undefined'){
+		    tracks.tracks = new Array();
+		}
 
-    if(currentTrack+1 > tracks.tracks.length)
-      currentTrack = 0;
-    else if(currentTrack < 0)
-      currentTrack = 0;
+		customTrackQueue.push(data);
+		tracks.tracks = customTrackQueue;
+		tracks.tracks[customTrackQueue.length-1].trackURI = id;
 
-    highlighTrackRow(tracks.tracks[currentTrack].trackURI);
-    renderHandlebarsTemplate('currentTrack', tracks.tracks[currentTrack], '.currentTrack');
-    renderAlbumArt(tracks.tracks[currentTrack].trackURI);          
-    $('#audioPlayer').attr('src','/'+tracks.tracks[currentTrack].trackURI+'.mp3');
-    $('#audioPlayer').get(0).load();                    
-  }
+		if(firstSong == true){
+		    currentTrack = 0;
+		    loadTrack();
+		}
 
-  highlighTrackRow = function(trackURI){
-    $('.table .alert-info').attr('class', 'success');
-    $('.table tr[id*='+trackURI.split(':')[2]+']').attr('class', 'alert-info');
-  }
+		$('.modal').modal('hide');
+	    });
+	}
+    });
 
-  playTrack = function(trackURI){
-    var trackNumber = $('tr[name="track"]').index($('tr[id*='+trackURI.split(':')[2]+']'));
-    currentTrack = trackNumber;
-    loadTrack();
-  }
+    getPlayLists = function() {
+	//Get Playlists
+	$.ajax({
+	    cache: false,
+	    dataType: "json",
+	    url: '/spotify-server/playlists',
+	    beforeSend: function() {
+		$('.modal').modal('show');
+	    }
+	}).done(function(data){
+	    if(data.error){
+		displayError(data.error);
+		return;
+	    }
 
-  //Render Handlebars Template
-  renderHandlebarsTemplate = function(templateId, context, targetId){
-    console.log('Rendering handlebars template with id',templateId,'and context',context);
-    var template = Handlebars.compile($('#'+templateId).html());    
-    var target = targetId || '.content';                
-    $(target).html(template(context));
-  }   
-
-  //Sort function for playlists
-  function sortByAttributeNameComparitor(a,b) {
-    if (a.attributes.name < b.attributes.name)
-      return -1;
-    if (a.attributes.name > b.attributes.name)
-      return 1;
-    
-    return 0;
-  }   
-
-  function displayError(error){
-    alert(error);
-  }
-
-  //Handler for login
-  $('#login').click(function(){
-    $.ajax({
-      cache: false,
-      dataType: "json",
-      url: '/spotify-server/login/'+$('#username').val()+':'+$('#password').val(),
-      beforeSend: function() {
-        $('.modal').modal('show');
-      }            
-    }).done(function(data){  
-      if(data.success){
-        $.cookie('username', $('#username').val());
-        $.cookie('password', $('#password').val());
-        $('#nav').show();
-        $('.login-wrapper').hide();        
-        getPlayLists();        
-      }
-      else{
-        $('#loginMessage').show();
-        $('.modal').modal('hide');
-      }       
-    }); 
-  });
-
-  //Handler for play
-  $('#audioPlayer').on('canplay', function(){             
-    $('#audioPlayer').get(0).play();
-    $('.modal').modal('hide');       
-  });  
-
-  //Handler for audio player progress
-  $('#audioPlayer').on('timeupdate', function(){    
-    var value = 0;
-    var audioPlayer = $('#audioPlayer').get(0);
-    var duration = tracks.tracks[currentTrack].duration/1000;
-
-    var s = (audioPlayer.currentTime % 60).toFixed(0);
-    var m = ((audioPlayer.currentTime / 60) % 60).toFixed(0);
-    var timePlayed = s < 10 ? m+':0'+s : m+':'+s;
-
-
-    var sleft = ((duration-audioPlayer.currentTime) % 60).toFixed(0);
-    var mleft = (((duration-audioPlayer.currentTime)/60) % 60).toFixed(0);
-    sleft = sleft < 10 ? '0'+sleft : sleft;
-    mleft = mleft < 10 ? '0'+mleft : mleft;
-
-    var played = mleft+':'+sleft;
-
-    if(audioPlayer.currentTime > 0){
-      value = ((100/duration) * audioPlayer.currentTime);
+	    data.playlists.sort(sortByAttributeNameComparitor)
+	    renderHandlebarsTemplate('playlists', data);
+	    playlists = data;
+	    $('.modal').modal('hide');
+	});
     }
-    $('#progress-bar').attr('aria-valuenow', value);
-    $('#progress-bar').attr('style', 'width: '+value+'%');
-    $('#current-time').text(timePlayed);
-    $('#duration').text(played);
 
-  }); 
+    //Get Plastlist tracks
+    getPlayListTracks = function(uri){
+	$.ajax({
+	    cache: false,
+	    dataType: "json",
+	    url: '/spotify-server/playlist/'+uri.split(':')[4],
+	    beforeSend: function() {
+		$('.modal').modal('show');
+	    }
+	}).done(function(data){
+	    if(data.error){
+		displayError(data.error);
+		return;
+	    }
 
-  //Handler for audio player track ended
-  $('#audioPlayer').on('ended', function() {
-    currentTrack++;
-    loadTrack();      
-  });   
+	    renderHandlebarsTemplate('tracks', data);
 
-  //Handler for Playlist link
-  $('#playlists-link').click(function(e){
-    e.preventDefault();
-    renderHandlebarsTemplate('playlists', playlists);
-  });    
-  $('.glyphicon-th-list').mouseover(function(){
-    $(this).tooltip('show');
-  });  
+	    //Load first track
+	    tracks = data;
+	    currentTrack = 0;
+	    loadTrack();
+	});
+    }
 
-  //Handler for Now playing link
-  $('#nowPlaying-link').click(function(e){
-    e.preventDefault();
-    renderHandlebarsTemplate('tracks', tracks);
-    renderHandlebarsTemplate('currentTrack', tracks.tracks[currentTrack], '.currentTrack');
-    renderAlbumArt(tracks.tracks[currentTrack].trackURI);   
-    highlighTrackRow(tracks.tracks[currentTrack].trackURI);
-  });
-  $('.glyphicon-play').mouseover(function(){
-    $(this).tooltip('show');
-  });   
+    renderAlbumArt = function(trackURI){
+	$.ajax({
+	    cache: false,
+	    dataType: "json",
+	    url: '/spotify-server/album-art/'+trackURI
+	}).done(function(data){
+	    renderHandlebarsTemplate('currentAlbumArt', data.oembed, '#albumArt');
+	});
+    }
 
-  //Handler for Search link
-  $('#search-link').click(function(e){
-    e.preventDefault();
-    renderHandlebarsTemplate('searchResults', null);
-  });
+    //Play next track
+    loadTrack = function() {
+	$('.modal').modal('show');
 
-  //Handler for Search
-  $('.search').keypress(function(e){
-    var self = $(this);
+	if(currentTrack+1 > tracks.tracks.length)
+	    currentTrack = 0;
+	else if(currentTrack < 0)
+	    currentTrack = 0;
 
-    if(e.which==13){
-      $.ajax({
-        cache: false,
-        dataType: "json",
-        url: '/spotify-server/search/'+self.val(),
-        beforeSend: function() {
-          $('.modal').modal('show');
-        }            
-      }).done(function(data){  
-        if(data.error){
-          displayError(data.error);
-          return;
-        }  
-               
-        renderHandlebarsTemplate('searchResults', data);
-        $('.modal').modal('hide'); 
-      });     
-    } 
-  });  
-});  
+	highlighTrackRow(tracks.tracks[currentTrack].trackURI);
+	renderHandlebarsTemplate('currentTrack', tracks.tracks[currentTrack], '.currentTrack');
+	renderAlbumArt(tracks.tracks[currentTrack].trackURI);
+	$('#audioPlayer').attr('src','/'+tracks.tracks[currentTrack].trackURI+'.mp3');
+	$('#audioPlayer').get(0).load();
+    }
+
+    highlighTrackRow = function(trackURI){
+	$('.table .alert-info').attr('class', 'success');
+	$('.table tr[id*='+trackURI.split(':')[2]+']').attr('class', 'alert-info');
+    }
+
+    playTrack = function(trackURI){
+	var trackNumber = $('tr[name="track"]').index($('tr[id*='+trackURI.split(':')[2]+']'));
+	currentTrack = trackNumber;
+	loadTrack();
+    }
+
+    //Render Handlebars Template
+    renderHandlebarsTemplate = function(templateId, context, targetId){
+	//console.log('Rendering handlebars template with id',templateId,'and context',context);
+	var template = Handlebars.compile($('#'+templateId).html());
+	var target = targetId || '.content';
+	$(target).html(template(context));
+    }
+
+    //Sort function for playlists
+    function sortByAttributeNameComparitor(a,b) {
+	if (a.attributes.name < b.attributes.name)
+	    return -1;
+	if (a.attributes.name > b.attributes.name)
+	    return 1;
+
+	return 0;
+    }
+
+    function displayError(error){
+	console.error(error);
+    }
+
+
+    $('#login').click(function(){
+	var d = $('#login-form').serialize();
+	$.ajax({
+	    data: d,
+	    type: 'POST',
+	    url: '/spotify-server/login',
+	    success: function(data) {
+		if (data.success) {
+		    $.cookie('username', $('#username').val());
+		    $.cookie('password', $('#password').val());
+		    $('#nav').show();
+		    $('.login-wrapper').hide();
+		    getPlayLists();
+		}
+		else{
+		    $('#loginMessage').show();
+		    $('.modal').modal('hide');
+		}
+	    }
+	});
+	/*$.post({
+	  cache: false,
+	  dataType: "json",
+	  url: '/spotify-server/login',
+	  beforeSend: function() {
+	  $('.modal').modal('show');
+	  }
+	  }).done(function(data){
+	  if(data.success){
+	  $.cookie('username', $('#username').val());
+	  $.cookie('password', $('#password').val());
+	  $('#nav').show();
+	  $('.login-wrapper').hide();
+	  getPlayLists();
+	  }
+	  else{
+	  $('#loginMessage').show();
+	  $('.modal').modal('hide');
+	  }
+	  })*/;
+    });
+
+    //Handler for play
+    $('#audioPlayer').on('canplay', function(){
+	$('#audioPlayer').get(0).play();
+	$('.modal').modal('hide');
+    });
+
+    //Handler for audio player progress
+    $('#audioPlayer').on('timeupdate', function(){
+	var value = 0;
+	var audioPlayer = $('#audioPlayer').get(0);
+	var duration = tracks.tracks[currentTrack].duration/1000;
+
+	var s = (audioPlayer.currentTime % 60).toFixed(0);
+	var m = ((audioPlayer.currentTime / 60) % 60).toFixed(0);
+	var timePlayed = s < 10 ? m+':0'+s : m+':'+s;
+
+
+	var sleft = ((duration-audioPlayer.currentTime) % 60).toFixed(0);
+	var mleft = (((duration-audioPlayer.currentTime)/60) % 60).toFixed(0);
+	sleft = sleft < 10 ? '0'+sleft : sleft;
+	mleft = mleft < 10 ? '0'+mleft : mleft;
+
+	var played = mleft+':'+sleft;
+
+	if(audioPlayer.currentTime > 0){
+	    value = ((100/duration) * audioPlayer.currentTime);
+	}
+	$('#progress-bar').attr('aria-valuenow', value);
+	$('#progress-bar').attr('style', 'width: '+value+'%');
+	$('#current-time').text(timePlayed);
+	$('#duration').text(played);
+
+    });
+
+    //Handler for audio player track ended
+    $('#audioPlayer').on('ended', function() {
+	currentTrack++;
+	loadTrack();
+    });
+
+    //Handler for Playlist link
+    $('#playlists-link').click(function(e){
+	e.preventDefault();
+	renderHandlebarsTemplate('playlists', playlists);
+    });
+    $('.glyphicon-th-list').mouseover(function(){
+	$(this).tooltip('show');
+    });
+
+    //Handler for Now playing link
+    $('#nowPlaying-link').click(function(e){
+	e.preventDefault();
+	renderHandlebarsTemplate('tracks', tracks);
+	renderHandlebarsTemplate('currentTrack', tracks.tracks[currentTrack], '.currentTrack');
+	renderAlbumArt(tracks.tracks[currentTrack].trackURI);
+	highlighTrackRow(tracks.tracks[currentTrack].trackURI);
+    });
+    $('.glyphicon-play').mouseover(function(){
+	$(this).tooltip('show');
+    });
+
+    //Handler for Search link
+    $('#search-link').click(function(e){
+	e.preventDefault();
+	renderHandlebarsTemplate('searchResults', null);
+    });
+
+    //Handler for Search
+    $('.search').keypress(function(e){
+	var self = $(this);
+
+	if(e.which==13){
+	    $.ajax({
+		cache: false,
+		dataType: "json",
+		url: '/spotify-server/search/'+self.val(),
+		beforeSend: function() {
+		    $('.modal').modal('show');
+		}
+	    }).done(function(data){
+		if(data.error){
+		    displayError(data.error);
+		    return;
+		}
+
+		renderHandlebarsTemplate('searchResults', data);
+		$('.modal').modal('hide');
+	    });
+	}
+    });
+});
--- spotify-server.js
+++ spotify-server.js
@@ -1,6 +1,7 @@
 var express = require('express'),
-    session = require('express-session');
-    cookieParser = require('cookie-parser');  
+    session = require('express-session'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
     http = require('http'),
     ejs = require('ejs'),
     path = require('path'),
@@ -9,8 +10,8 @@ var express = require('express'),
 
 //Don't stop this server if an exception goes uncaught
 process.on('uncaughtException', function (err) {
-  console.error((err.stack+'').red.bold);
-  console.error('Node trying not to exit...'.red.bold);
+    console.error((err.stack+'').red.bold);
+    console.error('Node trying not to exit...'.red.bold);
 });     
 
 var app = express();
@@ -18,121 +19,120 @@ app.set('port', 3000);
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
-app.use(cookieParser('asdklfjl43u8t943htinjgkrenseiro3u8urijoewfdkls'));
-app.use(session());
+app.use(session({ secret: 'keyboard cat loves spotify', cookie: { maxAge: 60000 }}));
+app.use(bodyParser.text({ type: 'text/html' }));
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 //Index
 app.get('/', function(req, res){
-  res.render('index');
+    res.render('index');
 });
 
 app.get('/spotify-server/logout', function(req,res){
-  req.session.destroy(function(){
-    res.redirect('/');
-  });
+    req.session.destroy(function(){
+	res.redirect('/');
+    });
 });
 
 //Get login information
-app.get('/spotify-server/login/:usernameAndpassword', function(req, res){
-  var up = req.params.usernameAndpassword;
-  var uname = up.split(':')[0];
-  var pw = up.split(':')[1];
+app.post('/spotify-server/login', function(req, res, next){
+    var uname = req.body.username;
+    var pw = req.body.password;
 
-  if(req.session.loggedin && uname=='check'){
-    res.send({success: 'success'});
-    return;
-  }
+    if(req.session.loggedin && uname=='check'){
+	res.send({success: 'success'});
+	return;
+    }
 
-  if(!req.session.loggedin && uname=='check'){
-    res.send({error: {message: "invalid login"}});
-    return;
-  }
+    if(!req.session.loggedin && uname=='check'){
+	res.send({error: {message: "invalid login"}});
+	return;
+    }
 
-  spotifyClient.newInstance(uname,pw).login()
-  .on('error', function(e){
-    res.send({error: {message: "invalid login"}});
-  })
-  .on('success', function(data){
-    req.session.username = uname;
-    req.session.password = pw;
-    req.session.loggedin = true;
-    res.send({success: 'success'});
-  });
+    spotifyClient.newInstance(uname,pw).login()
+	.on('error', function(e){
+	    res.send({error: {message: "invalid login"}});
+	})
+	.on('success', function(data){
+	    req.session.username = uname;
+	    req.session.password = pw;
+	    req.session.loggedin = true;
+	    res.send({success: 'success'});
+	});
 });
 
 //Retrieve PlayLists
 app.get('/spotify-server/playlists', function(req, res){
-  spotifyClient.newInstance(req.session.username,req.session.password).getPlayLists()
-  .on('playListsReady', function(playlists){
-    res.send({playlists: playlists});
-  })
-  .on('error', function(err){
-    res.send({error: err});
-  });
+    spotifyClient.newInstance(req.session.username,req.session.password).getPlayLists()
+	.on('playListsReady', function(playlists){
+	    res.send({playlists: playlists});
+	})
+	.on('error', function(err){
+	    res.send({error: err});
+	});
 });
 
 //Retrieve tracks for a given PlayList
 app.get('/spotify-server/playlist/:playlistid', function(req, res){
-  var playlistid = req.params.playlistid;
-  var uri = 'spotify:user:'+req.session.username+':playlist:'+playlistid;
+    var playlistid = req.params.playlistid;
+    var uri = 'spotify:user:'+req.session.username+':playlist:'+playlistid;
 
-  spotifyClient.newInstance(req.session.username,req.session.password).getTracksByPlayListURI(uri)
-  .on('tracksReady', function(tracks){
-    res.send({tracks: tracks});
-  })
-  .on('error', function(err){
-    res.send({error: err});
-  }); 
+    spotifyClient.newInstance(req.session.username,req.session.password).getTracksByPlayListURI(uri)
+	.on('tracksReady', function(tracks){
+	    res.send({tracks: tracks});
+	})
+	.on('error', function(err){
+	    res.send({error: err});
+	}); 
 });
 
 //Retrieve a single track
 app.get('/spotify-server/track/:trackURI', function(req, res){
-  var uri = req.params.trackURI;
+    var uri = req.params.trackURI;
 
-  spotifyClient.newInstance(req.session.username,req.session.password).getTrackByTrackURI(uri)
-  .on('trackReady', function(track){
-    res.send(track);
-  })
-  .on('error', function(err){
-    res.send({error: err});
-  });
+    spotifyClient.newInstance(req.session.username,req.session.password).getTrackByTrackURI(uri)
+	.on('trackReady', function(track){
+	    res.send(track);
+	})
+	.on('error', function(err){
+	    res.send({error: err});
+	});
 });
 
 //Retreive album art for a given track
 app.get('/spotify-server/album-art/:trackURI', function(req, res){
-  var trackURI = req.params.trackURI;
+    var trackURI = req.params.trackURI;
 
-  spotifyClient.newInstance(req.session.username,req.session.password).getAlbumArtByTrackURI(trackURI)
-  .on('albumArtReady'+trackURI, function(data){
-    res.send(data);
-  })
-  .on('error', function(err){
-    res.send({error: err});
-  });
+    spotifyClient.newInstance(req.session.username,req.session.password).getAlbumArtByTrackURI(trackURI)
+	.on('albumArtReady'+trackURI, function(data){
+	    res.send(data);
+	})
+	.on('error', function(err){
+	    res.send({error: err});
+	});
 });
 
 //Search (tracks only right now)
 app.get('/spotify-server/search/:query', function(req,res){
-  var query = req.params.query;
-  spotifyClient.newInstance(req.session.username,req.session.password).search(query)
-  .on('searchResultsReady', function(data){
-    res.send(data);
-  })
-  .on('searchResultsError', function(e){
-    res.send({error: 'searchResultsError'});
-  })
+    var query = req.params.query;
+    spotifyClient.newInstance(req.session.username,req.session.password).search(query)
+	.on('searchResultsReady', function(data){
+	    res.send(data);
+	})
+	.on('searchResultsError', function(e){
+	    res.send({error: 'searchResultsError'});
+	})
 });
 
 //Play a track
 app.get('/:trackId.mp3', function(req, res){
-  var trackURI = req.params.trackId;
+    var trackURI = req.params.trackId;
 
-  //Just pass the response here because we need to stream to it
-  spotifyClient.newInstance(req.session.username,req.session.password).playTrackByURI(trackURI, res); 
+    //Just pass the response here because we need to stream to it
+    spotifyClient.newInstance(req.session.username,req.session.password).playTrackByURI(trackURI, res); 
 });
-
 
 var server = http.createServer(app);
 server.listen(app.get('port'), function(){
-  console.log('Listening on port',app.get('port'));
+    console.log('Listening on port',app.get('port'));
 });
